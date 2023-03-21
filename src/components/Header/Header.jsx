01f2bf5c4303f70b22ee72983b658c383742fb3e
Aliaksandra <alekspekhota@gmail.com>
2023-03-21 15:45:22 +0100

    feat: create index file in components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,18 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
 
-export default function Header() {
+export function Header() {
   return (
     <header>
       <nav className={styles.header__menu}>
         <ul className={styles.menu__list}>
           <li className={styles.list__item}>
-            <NavLink to="" className={styles.link}>
+            <NavLink to="/" className={styles.link}>
               Page 1
             </NavLink>
           </li>
           <li className={styles.list__item}>
-            <NavLink to="" className={styles.link}>
+            <NavLink to="/todo" className={styles.link}>
               Page 2
             </NavLink>
           </li>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
-import Header from "../Header/Header";
+import { Header } from "../index";
 
-export default function Layout() {
+export function Layout() {
   return (
     <>
       <Header />
