7b382d4b6623736135d9cb55591e5682f8dc765d
Aliaksandra <alekspekhota@gmail.com>
2023-03-22 15:19:06 +0100

    feat: add changeActive for tasks

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,17 +1,33 @@
 import { MdDeleteOutline } from "react-icons/md";
 import { AiOutlineEdit } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { changeActive } from "../../features/todos.slice";
 import styles from "./TodoItem.module.css";
 
 export function TodoItem({ task }) {
+  const dispatch = useDispatch();
   function checkStyle() {
-    if (task.active === false) {
+    if (!task.active) {
       return `${styles.task} ${styles.inactive}`;
     }
     return `${styles.task}`;
   }
+
+  function handleClick() {
+    dispatch(changeActive(task.id));
+  }
+
   return (
     <div className={checkStyle()}>
-      <p className={styles.task__description}>{task.name}</p>
+      <div
+        className={styles.description__block}
+        role="button"
+        tabIndex={0}
+        onClick={handleClick}
+        onKeyDown={handleClick}
+      >
+        <p className={styles.task__description}>{task.name}</p>
+      </div>
       <div className={styles.task__controllers}>
         <MdDeleteOutline
           color="red"
--- src/features/todos.slice.js
+++ src/features/todos.slice.js
@@ -1,7 +1,9 @@
+import todoList from "../data/todoList";
+
 const { createSlice } = require("@reduxjs/toolkit");
 
 const initialState = {
-  todoList: [],
+  todoList,
 };
 
 export const todosSlice = createSlice({
@@ -11,9 +13,17 @@ export const todosSlice = createSlice({
     addTodo: (state, action) => {
       state.todoList = [...state.todoList, action.payload];
     },
+    changeActive: (state, action) => {
+      state.todoList = state.todoList.map((todo) => {
+        if (todo.id === action.payload) {
+          return { ...todo, active: !todo.active };
+        }
+        return todo;
+      });
+    },
   },
 });
 
-export const { addTodo } = todosSlice.actions;
+export const { addTodo, changeActive } = todosSlice.actions;
 
 export default todosSlice.reducer;
