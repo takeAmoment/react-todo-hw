4c88ebb6286cbaef1e2c618c020be94b94002084
Aliaksandra <alekspekhota@gmail.com>
2023-03-23 11:01:59 +0100

    feat: show page 2 if username exist

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,35 @@
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
 
 export function Header() {
+  const { username } = useSelector((state) => state.username);
   return (
     <header>
       <nav className={styles.header__menu}>
         <ul className={styles.menu__list}>
           <li className={styles.list__item}>
-            <NavLink to="/" className={styles.link}>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? `${styles.link} ${styles.active}` : styles.link
+              }
+            >
               Page 1
             </NavLink>
           </li>
-          <li className={styles.list__item}>
-            <NavLink to="/todo" className={styles.link}>
-              Page 2
-            </NavLink>
-          </li>
+          {username && (
+            <li className={styles.list__item}>
+              <NavLink
+                to="/todo"
+                className={({ isActive }) =>
+                  isActive ? `${styles.link} ${styles.active}` : styles.link
+                }
+              >
+                Page 2
+              </NavLink>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
